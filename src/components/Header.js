85640a0f5e33936f189d13983a80277fdc133eb4
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-07-09 22:50:22 -0400

    Added images

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import logo from "../images/logo.svg"
+
+
+
+export default function Header(){
+    return(
+        <div>
+            <img src={logo} alt="lolo" className="logo" />
+        </div>
+    )
+}
+
+
