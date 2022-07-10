d2ffb8e065df50e66b67a3b8191f1db9983b9eea
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-07-09 23:21:44 -0400

    Completed Mobile

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import facebook from "../images/facebook.svg"
+import twitter from "../images/twitter.svg"
+import instagram from "../images/instagram.svg" 
+
+
+
+ export default function Footer(){
+    return(
+        <footer>
+            <img src={facebook} alt="facebook-logo" className="logo-footer"/>
+            <img src={twitter} alt="twitter" className="logo-footer"/>
+            <img src={instagram} alt="instagram" className="logo-footer"/>
+        </footer>
+    )
+ }
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+
+
+
+export default function main(){
+    return(
+        <main>
+            <div className="main-img-mobile  main-img-desktop"></div>
+            <div className="main-content">
+                <h2>Build The Community Your Fans Will Love</h2>
+                <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine dissusion.</p>
+            </div>
+            <button>Register</button>
+        </main>
+    )
+}
